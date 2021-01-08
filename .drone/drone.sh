2dff3e397564af4f42cb35e47ad098e243820395
sdarwin <samuel.d.darwin@gmail.com>
2021-01-08 14:37:53 +0000

    add drone config [ci skip]

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-set -ex
+# Copyright 2020 Rene Rivera, Sam Darwin
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+set -e
 export TRAVIS_BUILD_DIR=$(pwd)
 export DRONE_BUILD_DIR=$(pwd)
 export TRAVIS_BRANCH=$DRONE_BRANCH
 export VCS_COMMIT_ID=$DRONE_COMMIT
 export GIT_COMMIT=$DRONE_COMMIT
+export REPO_NAME=$DRONE_REPO
 export PATH=~/.local/bin:/usr/local/bin:$PATH
 
-echo '==================================> BEFORE_INSTALL'
-
-. .drone/before-install.sh
+if [ "$DRONE_JOB_BUILDTYPE" == "boost" ]; then
 
 echo '==================================> INSTALL'
 
@@ -24,15 +27,9 @@ git rm --ignore-unmatch libs/multi_index/
 mkdir libs/multi_index
 cp -r $TRAVIS_BUILD_DIR/* libs/multi_index
 
-echo '==================================> BEFORE_SCRIPT'
-
-. $DRONE_BUILD_DIR/.drone/before-script.sh
-
 echo '==================================> SCRIPT'
 
 echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
 ./b2 libs/multi_index/test toolset=$TOOLSET cxxstd=$CXXSTD
 
-echo '==================================> AFTER_SUCCESS'
-
-. $DRONE_BUILD_DIR/.drone/after-success.sh
+fi
